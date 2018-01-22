47a9a8e72a85cb99373e23c9557f1903b5bf2578
ighmouraceneb <ighmouraceneb@gmail.com>
2018-01-22 11:27:55 +0100

    Animation jquery et css pour la partie skills home

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,12 +3,32 @@ $('document').ready(function()
 
     $(".integration").click(function () {
         $(".presentation-integration").slideToggle();
+        $(".presentation-developpement").hide('slow');
+        $(".presentation-outils").hide('show');
+        $(this).toggleClass('skill-title-color');
+        $(".developpement").removeClass('skill-title-color');
+        $(".outil").removeClass('skill-title-color');
     });
     $(".developpement").click(function () {
         $(".presentation-developpement").slideToggle();
+        $(".presentation-integration").hide('show');
+        $(".presentation-outils").hide('show');
+        $(this).toggleClass('skill-title-color');
+        $(".integration").removeClass('skill-title-color');
+        $(".outil").removeClass('skill-title-color');
     });
     $(".outil").click(function () {
         $(".presentation-outils").slideToggle();
+        $(".presentation-integration").hide('show');
+        $(".presentation-developpement").hide('show');
+        $(this).toggleClass('skill-title-color');
+         $(".integration").removeClass('skill-title-color');
+        $(".developpement").removeClass('skill-title-color');
+    });
+    
+
+    $(".btn-skill").hover(function(){
+        $(this).toggleClass('btn-skill-animate');
     });
 
 });
