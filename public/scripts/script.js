2c62d5011026da2f3311245d463299091b83a6d6
ighmouraceneb <ighmouraceneb@gmail.com>
2018-01-27 13:12:19 +0100

    Finalisation de la partie skill dans le home

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,32 +1,16 @@
 $('document').ready(function()
  {
+/**
+* @description Sytème d'accordéon : développer/réduire le contenu en cliquant * sur les en-têtes + changement de l'affichage des en-tête au développement et * au réduction
+* */  
+    $(".skill-title").click(function(){
 
-    $(".integration").click(function () {
-        $(".presentation-integration").slideToggle();
-        $(".presentation-developpement").hide('slow');
-        $(".presentation-outils").hide('slow');
+        $(this).next().slideToggle();
         $(this).toggleClass('skill-title-color');
-        $(".developpement").removeClass('skill-title-color');
-        $(".outil").removeClass('skill-title-color');
     });
-    $(".developpement").click(function () {
-        $(".presentation-developpement").slideToggle();
-        $(".presentation-integration").hide('slow');
-        $(".presentation-outils").hide('slow');
-        $(this).toggleClass('skill-title-color');
-        $(".integration").removeClass('skill-title-color');
-        $(".outil").removeClass('skill-title-color');
-    });
-    $(".outil").click(function () {
-        $(".presentation-outils").slideToggle();
-        $(".presentation-integration").hide('slow');
-        $(".presentation-developpement").hide('slow');
-        $(this).toggleClass('skill-title-color');
-         $(".integration").removeClass('skill-title-color');
-        $(".developpement").removeClass('skill-title-color');
-    });
-    
-
+/**
+* @description Une animation au survole d'un botton
+* */ 
     $(".btn-skill").hover(function(){
         $(this).toggleClass('btn-skill-animate');
     });
